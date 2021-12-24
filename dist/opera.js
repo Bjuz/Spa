caea4aef89c4ab6ebc7b01423cd7c743b0d0f21f
Bjuz <81210725+Bjuz@users.noreply.github.com>
2021-12-24 16:35:00 -0600

    Changes

---
+++ dist/opera.js
@@ -0,0 +1,14 @@
+const check =  document.getElementById('checkboxP');
+
+
+
+
+function Ir(){
+	const audio = new Audio('../img/mis.mp3');
+	audio.play();
+}
+
+function No(){
+	const audio = new Audio('../img/mis.mp3');
+	audio.play();
+}
